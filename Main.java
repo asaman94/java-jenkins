dcc8659bff15861e4210bad711d22dbdf65f8767
Aman Sharma <asaman94@gmail.com>
2025-01-05 14:23:45 +0530

    Initial commit

---
+++ Main.java
@@ -0,0 +1,10 @@
+import java.util.Date;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("This is Java Program");
+        System.out.println("Output from Java");
+        System.out.println(new Date());
+    }
+}
