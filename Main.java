fd1bdabccee41b3b2bc773fc4d87636a5cfe9995
Aman Sharma <asaman94@gmail.com>
2025-01-05 15:35:45 +0530

    added st class

--- Main.java
+++ Main.java
@@ -2,11 +2,17 @@ import java.util.Date;
 
 public class Main {
 
+    
+
     public static void main(String[] args) {
         System.out.println("This is Java Program");
         System.out.println("Output from Java");
         System.out.println(new Date());
         System.out.println("Hello");
         System.out.println("Add one more file");
+        System.out.println("Amazing job");
+
+        Student student=new Student();
+        student.sayHello();
     }
 }
---
+++ Student.java
@@ -0,0 +1,9 @@
+public class Student {
+    public Student(){
+        System.out.println("Crating student object");
+    }
+
+    public void sayHello(){
+        System.out.println("Hi, I am student");
+    }
+}
